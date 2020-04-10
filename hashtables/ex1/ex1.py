21ca1f32bc597923773626f7122f19a3bcc9bee3
Ali Rajaii <rajaii_a@yahoo.com>
2020-04-10 11:43:04 -0400

    hastables done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -6,6 +6,10 @@ from hashtables import (HashTable,
                         hash_table_resize)
 
 
+# input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+# output: [ 3, 1 ]  # since these are the indices of weights 15 
+# and 6 whose sum equals 21     21- 15 = 6 == target weigts[i] == 15
+
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
@@ -13,7 +17,24 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
 
+    for i in range(len(weights) - 1):
+        hash_table_insert(ht, weights[i], i)
+        if weights[i+1] == weights[i]:
+            hash_table_insert(ht, weights[i+1], i+1)
+            return (1, 0)
+        print(f'just inserted <{weights[i]},{i}>')
+        target = limit - weights[i]
+        print(f'target: {target}')
+        print(f'<target: {target}, weightsi: {weights[i]}, limit: {limit}>')
+        if hash_table_retrieve(ht, target) or hash_table_retrieve(ht, target) == 0:
+            if hash_table_retrieve(ht, target) >= i:
+                return (hash_table_retrieve(ht, target), i)
+            elif hash_table_retrieve(ht, target) < i:
+                return (i, hash_table_retrieve(ht, target))
+
     return None
+
+
 
 
 def print_answer(answer):
@@ -21,3 +42,5 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+print(get_indices_of_item_weights([4,4], 2, 8))
